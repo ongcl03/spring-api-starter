af32abf46e2b32bcb3ffbc9bfdd3f86d676d1208
CL <ongchangle03@gmail.com>
2025-09-06 12:29:26 +0800

    Using Data Transfer Object (DTO)

---
+++ src/main/java/com/codewithmosh/store/dtos/UserDto.java
@@ -0,0 +1,13 @@
+package com.codewithmosh.store.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class UserDto {
+    private long id;
+    private String name;
+    private String email;
+
+}
