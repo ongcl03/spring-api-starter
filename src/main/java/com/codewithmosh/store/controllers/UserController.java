02058021d7514e5f7553a42c27f7cb94cd5b43d9
CL <ongchangle03@gmail.com>
2025-09-06 16:21:06 +0800

    Extracting Request Body

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -23,11 +23,9 @@ public class UserController {
 
     @GetMapping("")
     public Iterable<UserDto> getAllUsers(
-            @RequestHeader(name = "x-auth-token", required = false) String authToken,
             @RequestParam(required = false, defaultValue = "", name = "sort") String sortBy
     ) {
 
-        System.out.println(authToken);
 
         if (!Set.of("name", "email").contains(sortBy)){
             sortBy =  "name";
@@ -50,6 +48,11 @@ public class UserController {
 
 
         return ResponseEntity.ok(userMapper.toDto(user));
+    }
+
+    @PostMapping
+    public UserDto createUser(@RequestBody UserDto data) {
+        return data;
     }
 
 }
